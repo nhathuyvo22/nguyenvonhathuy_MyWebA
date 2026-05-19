b65f325716500f9852c1e0f5b00f0cb7b29d0110
nhathuyvo22 <nhathuyvo2204@gmail.com>
2026-05-19 16:02:32 +0700

    Laravel Lab3

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,47 @@ Route::get('/demo3', [DemoController::class, 'index3']);
 Route::get('/demo4/{id}', [DemoController::class, 'index4']);
 Route::get('/demo5/{id?}', [DemoController::class, 'index5']);
 Route::get('/demo6/{param1}/{param2}', [DemoController::class, 'index6']);
-//Category
-Route::resource('admin/category', CategoryController::class);
-Route::resource('admin/product', ProductController::class);
-Route::resource('admin/brand', BrandController::class);
-Route::resource('admin/user', UserController::class);
-Route::resource('admin/post', PostController::class);
+// Category
+Route::get('admin/category', [CategoryController::class, 'index']);
+Route::get('admin/category/create', [CategoryController::class, 'create']);
+Route::post('admin/category', [CategoryController::class, 'store']);
+Route::get('admin/category/{id}', [CategoryController::class, 'show']);
+Route::get('admin/category/{id}/edit', [CategoryController::class, 'edit']);
+Route::put('admin/category/{id}', [CategoryController::class, 'update']);
+Route::delete('admin/category/{id}', [CategoryController::class, 'destroy']);
+
+// Product
+Route::get('admin/product', [ProductController::class, 'index']);
+Route::get('admin/product/create', [ProductController::class, 'create']);
+Route::post('admin/product', [ProductController::class, 'store']);
+Route::get('admin/product/{id}', [ProductController::class, 'show']);
+Route::get('admin/product/{id}/edit', [ProductController::class, 'edit']);
+Route::put('admin/product/{id}', [ProductController::class, 'update']);
+Route::delete('admin/product/{id}', [ProductController::class, 'destroy']);
+
+// Brand
+Route::get('admin/brand', [BrandController::class, 'index']);
+Route::get('admin/brand/create', [BrandController::class, 'create']);
+Route::post('admin/brand', [BrandController::class, 'store']);
+Route::get('admin/brand/{id}', [BrandController::class, 'show']);
+Route::get('admin/brand/{id}/edit', [BrandController::class, 'edit']);
+Route::put('admin/brand/{id}', [BrandController::class, 'update']);
+Route::delete('admin/brand/{id}', [BrandController::class, 'destroy']);
+
+// User
+Route::get('admin/user', [UserController::class, 'index']);
+Route::get('admin/user/create', [UserController::class, 'create']);
+Route::post('admin/user', [UserController::class, 'store']);
+Route::get('admin/user/{id}', [UserController::class, 'show']);
+Route::get('admin/user/{id}/edit', [UserController::class, 'edit']);
+Route::put('admin/user/{id}', [UserController::class, 'update']);
+Route::delete('admin/user/{id}', [UserController::class, 'destroy']);
+
+// Post
+Route::get('admin/post', [PostController::class, 'index']);
+Route::get('admin/post/create', [PostController::class, 'create']);
+Route::post('admin/post', [PostController::class, 'store']);
+Route::get('admin/post/{id}', [PostController::class, 'show']);
+Route::get('admin/post/{id}/edit', [PostController::class, 'edit']);
+Route::put('admin/post/{id}', [PostController::class, 'update']);
+Route::delete('admin/post/{id}', [PostController::class, 'destroy']);
